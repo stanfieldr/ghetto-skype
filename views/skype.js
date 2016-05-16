914d4f36b0231fd2808cabbfba6e5c6b6fc7c3e2
Ryan Clancy <ryanxclancy@gmail.com>
2016-05-15 21:43:11 -0300

    Add support for calling.
    As mentioned in a Reddit post (https://www.reddit.com/r/linux/comments/4ji92o/finally_got_skype_for_web_to_work_on_linux/), the web version of Skype no longer needs a plugin for Edge - this means we can fake the user agent and support calling.

--- views/skype.js
+++ views/skype.js
@@ -49,7 +49,10 @@ skypeView.addEventListener('dom-ready', function boot() {
 		}, () => {});
 	}
 
-	skypeView.loadURL('https://web.skype.com/en');
+	skypeView.loadURL('https://web.skype.com/en', {
+		userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'
+	});
+
 });
 
 skypeView.addEventListener('did-navigate', function() {
