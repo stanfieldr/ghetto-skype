3f9bc2c39048ec627c8b9b89b187e4f4867cc9ca
Robert Stanfield <robert@stanfield.it>
2016-12-17 18:25:54 -0500

    Fixed audio calls

--- src/main.js
+++ src/main.js
@@ -69,6 +69,8 @@ app.on('ready', () => {
 	});
 
 	TrayIcon.init(mainWindow);
+
+	mainWindow.webContents.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
 	mainWindow.loadURL('https://web.skype.com/en');
 });
 
